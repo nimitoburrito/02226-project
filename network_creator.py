16d764d8829d8a65cb5c29a6791de32d18391ae0
nimitoburrito <nimit.parekh1@gmail.com>
2024-11-07 17:04:03 +0100

    fixed issue where first line was being skipped

--- network_creator.py
+++ network_creator.py
@@ -16,7 +16,7 @@ def create_graph_from_csv(file_path):
             if parts[0] in ['SW', 'ES']:
                 device_type = parts[0]
                 device_name = parts[1]
-                ports = parts[2] if len(parts) > 2 else None
+                ports = parts[2]
                 domain = parts[3] if len(parts) > 3 else None
                 
                 # Add node to the graph with attributes
@@ -54,31 +54,30 @@ def find_shortest_paths_for_streams(graph, streams_file_path):
     stream_paths = {};  # Dictionary to store the paths for each stream
     # Open the streams CSV file
     with open(streams_file_path, 'r') as file:
-        csv_reader = csv.reader(file)
-        next(csv_reader)  # Skip header row
+       for line in file:
         
-        
+        row = line.strip().split(',')
         # For each stream, find the shortest path and print it
-        for row in csv_reader:
-            pcp = row[0]
-            stream_name = row[1]
-            stream_type = row[2]
-            source_node = row[3]
-            destination_node = row[4]
-            size = row[5]
-            period = row[6]
-            deadline = row[7]
+
+        pcp = row[0]
+        stream_name = row[1]
+        stream_type = row[2]
+        source_node = row[3]
+        destination_node = row[4]
+        size = row[5]
+        period = row[6]
+        deadline = row[7]
             
-            try:
-                # Find the shortest path between source and destination
-                path = nx.shortest_path(graph, source=source_node, target=destination_node)
-                # Store the stream and the corresponding shortest path in the dictionary
-                stream_paths[stream_name] = path
-                #print(f"Path for stream {stream_name}: {' -> '.join(path)}")
-            except nx.NetworkXNoPath:
-                # If no path is found, store None or a message indicating no path found
-                stream_paths[stream_name] = None
-                print(f"No path found for stream {stream_name} from {source_node} to {destination_node}")
+        try:
+            # Find the shortest path between source and destination
+            path = nx.shortest_path(graph, source=source_node, target=destination_node)
+            # Store the stream and the corresponding shortest path in the dictionary
+            stream_paths[stream_name] = path
+            #print(f"Path for stream {stream_name}: {' -> '.join(path)}")
+        except nx.NetworkXNoPath:
+            # If no path is found, store None or a message indicating no path found
+            stream_paths[stream_name] = None
+            print(f"No path found for stream {stream_name} from {source_node} to {destination_node}")
 
     print(f"Stream Paths Dictionary: {stream_paths}")
     return stream_paths
