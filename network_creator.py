f9ad6e315b4ca8423cd7024695ec16cfe69b0230
nimitoburrito <nimit.parekh1@gmail.com>
2024-12-05 17:10:37 +0100

    cleaned up comments

--- network_creator.py
+++ network_creator.py
@@ -66,10 +66,6 @@ def create_graph_from_csv(file_path):
 
     return G
 
-# Example usage: call the function with your CSV file
-# create_graph_from_csv('example_topology.csv')
-#create_graph_from_csv('small-topology.csv')
-
 # This function assumes that the graph is already created
 def find_shortest_paths_for_streams(graph, streams_file_path):
     
@@ -95,13 +91,11 @@ def find_shortest_paths_for_streams(graph, streams_file_path):
             path = nx.shortest_path(graph, source=source_node, target=destination_node)
             # Store the stream and the corresponding shortest path in the dictionary
             stream_paths[stream_name] = path
-            #print(f"Path for stream {stream_name}: {' -> '.join(path)}")
+            
         except nx.NetworkXNoPath:
             # If no path is found, store None or a message indicating no path found
             stream_paths[stream_name] = None
-            #print(f"No path found for stream {stream_name} from {source_node} to {destination_node}")
 
-    #print(f"Stream Paths Dictionary: {stream_paths}")
     return stream_paths
 
 def get_streams_by_name(streams_file_path, name):
@@ -127,19 +121,16 @@ def get_streams_by_name(streams_file_path, name):
 
 
 
-# Example usage:
-# 1. First create the graph from your topology CSV file
-G = create_graph_from_csv(topology_file)  # Load your network graph
+# 1. First create the graph from topology CSV file
+G = create_graph_from_csv(topology_file)  # Load network graph
 
-# 2. Then, call the function to find paths for each stream
+# 2. function call to find paths for each stream
 paths = find_shortest_paths_for_streams(G, streams_file)
 
 for path in paths:
     for node in paths[path]:
         if type(node) is Switch:
             node.add_frame(get_streams_by_name(streams_file, path), prev_node)
-            #print(get_streams_by_name('small-streams.csv', path))
-        #print(node)
         prev_node=node
 
 sums = []
@@ -150,22 +141,13 @@ for path in paths:
     current_path = ""
     for node in paths[path]:
         if type(node) is Switch:
-            #print(node.calculate_delay(get_streams_by_name('small-streams.csv', path),prev_node))
             current_sums += node.calculate_delay(get_streams_by_name(streams_file, path),prev_node)
-            #print(get_streams_by_name('small-streams.csv', path))
             current_path += node.id
         else:
             current_path += node
-        #print(node)
         current_path += "->"
         prev_node=node
-
 
     sums.append(current_sums)
     line = path + ', ' + str(current_sums) + ', ' + get_streams_by_name(streams_file, path)[7] + ", " + current_path[:-2] + "\n"
     f.write(line)
-
-#print(sums)
-#for node in G:
-#    if type(node) is Switch:
-#        print(node.all_frames)
