8d42562ca5df0069104b78505f2c05488162d7a8
Jonas <jonas@wentzlau.dk>
2024-12-05 16:03:15 +0100

    The file is given in bytes, the calculation should use bits

--- switch.py
+++ switch.py
@@ -30,25 +30,25 @@ class Switch:
         b_H_hat = 0
         r_H_hat = 0
         for frame in higher_priorities:
-            b_H_hat += int(frame[5])
-            r_H_hat += int(frame[5])/int(frame[6])
+            b_H_hat += (int(frame[5])/8)
+            r_H_hat += (int(frame[5])/8)/int(frame[6])
         b_C_j_hat = 0
         for frame in same_priorities:
-            b_C_j_hat += int(frame[5])
+            b_C_j_hat += (int(frame[5])/8)
 
-        b_j_hat = int(frame_j[5])
+        b_j_hat = int(frame_j[5])/8
 
-        l_j_breve = int(frame_j[5])
+        l_j_breve = int(frame_j[5])/8
         l_L_hat = 0
 
         if lower_priorities:
-            l_L_hat = int(lower_priorities[0][5])
+            l_L_hat = int(lower_priorities[0][5])/8
 
         for frame in lower_priorities:
             if int(frame[5]) > l_L_hat:
-                l_L_hat = int(frame[5])
+                l_L_hat = int(frame[5])/8
 
-        r = 1000000000
+        r = 1000000
 
 
         return (b_H_hat + b_C_j_hat + b_j_hat - l_j_breve + l_L_hat)/(r-r_H_hat) + (l_j_breve/r)
